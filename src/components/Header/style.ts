ab321be3934144d34f67394bf3c7bdc27ca64a42
SEOKKAMONI <sj01048260279@gmail.com>
2023-01-08 11:54:41 +0900

    ADD :: Header Frame

--- src/components/Header/style.ts
+++ src/components/Header/style.ts
@@ -1,3 +1,8 @@
 import styled from 'styled-components';
 
-export const Header = styled.div``;
+export const Header = styled.div`
+  height: 60px;
+  width: 100%;
+  background-color: #fff;
+  border-bottom: 1px solid #e2e2e2;
+`;
