9a2ef4b95ddade078279e5ff58e1ebe78c4ddce3
SEOKKAMONI <sj01048260279@gmail.com>
2023-01-10 17:01:55 +0900

    ADD :: e-6 chart js

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import E1 from './components/e-1';
 import E2 from './components/e-2';
-import E5 from './components/e-5';
+import E6 from './components/e-6';
 
 import Svg from './components/Svg';
 
 function App() {
-  return <E5 />;
+  return <E6 />;
 }
 
 export default App;
