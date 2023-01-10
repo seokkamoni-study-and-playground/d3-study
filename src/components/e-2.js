85ab8054646285b0811198ae0db78411da67bac0
SEOKKAMONI <sj01048260279@gmail.com>
2023-01-10 12:19:17 +0900

    ADD :: 애니메이션 , getMaxValueIndex , Text 추가

--- src/components/e-2.js
+++ src/components/e-2.js
@@ -2,6 +2,18 @@ import * as d3 from 'd3';
 import React, { useEffect, useRef } from 'react';
 import styled from 'styled-components';
 
+const getMaxValueIndex = (arr) => {
+  let currentMaxValue = -1;
+  let maxIdx = 0;
+  arr.forEach((item, index) => {
+    if (currentMaxValue < item) {
+      currentMaxValue = item;
+      maxIdx = index;
+    }
+  });
+  return maxIdx;
+};
+
 export default function E2() {
   const canvasRef = useRef();
   const data = [100, 10, 30, 50, 110, 80];
@@ -16,10 +28,27 @@ export default function E2() {
     data.forEach((element, index) => {
       svg
         .append('rect')
+        .attr('id', `rect-${index}`)
         .attr('width', 20)
-        .attr('height', element)
+        .attr('height', 0)
         .attr('x', index * 30)
-        .attr('y', 120 - element);
+        .attr('y', 200)
+        .transition()
+        .duration(500)
+        .attr('height', element)
+        .attr('y', 200 - element);
+
+      d3.select(`#rect-${getMaxValueIndex(data)}`)
+        .transition()
+        .duration(1000)
+        .style('fill', '#0000ff')
+        .delay(500);
+
+      svg
+        .append('text')
+        .text(element)
+        .attr('x', index * 30)
+        .attr('y', 190 - element);
     });
   }, []);
 
