0425caa3acf7e9f221ebb22d4d937a2e7c2695ee
SEOKKAMONI <sj01048260279@gmail.com>
2023-01-10 14:26:09 +0900

    ADD :: e-4 원그래프

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,12 @@
 import React from 'react';
 import E1 from './components/e-1';
 import E2 from './components/e-2';
-import E3 from './components/e-3';
+import E4 from './components/e-4';
 
 import Svg from './components/Svg';
 
 function App() {
-  return (
-    <>
-      {/* <Svg /> */}
-      <E3 />
-    </>
-  );
+  return <E4 />;
 }
 
 export default App;
---
+++ src/components/e-4.js
@@ -0,0 +1,29 @@
+import * as d3 from 'd3';
+import React, { useEffect, useRef } from 'react';
+import styled from 'styled-components';
+
+export default function E4() {
+  const canvasRef = useRef();
+  const data = [100, 10, 110, 50, 40, 80];
+
+  useEffect(() => {
+    const canvas = d3.select(canvasRef.current);
+    const svg = canvas.append('svg').attr('width', 500).attr('height', 500);
+
+    const arcGenerator = d3.arc().innerRadius(50).outerRadius(100);
+    const pieGenerator = d3.pie();
+
+    const g = svg.append('g').attr('transform', 'translate(150,150)');
+
+    g.selectAll('path')
+      .data(pieGenerator(data))
+      .enter()
+      .append('path')
+      .attr('fill', 'brown')
+      .attr('stroke', 'black')
+      .attr('stroke-width', 4)
+      .attr('d', arcGenerator);
+  }, []);
+
+  return <div ref={canvasRef} />;
+}
